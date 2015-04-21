072e56bdf5b02af531004fff977a4f27f190d75b
llIlIllIlIllIlIllIlIllIlIIlIIllIIllIllI <jrf9t9@mail.missouri.edu>
2015-04-21 09:46:05 -0500

    Update paste.js
    Makin' it easier to fat

--- paste.js
+++ paste.js
@@ -1,4 +1,6 @@
 //  Paste this at the very bottom (append) of the '<RESFolder>/<version>/modules/keyboardNav.js'
+
+//  OS X:
 //  If you're on a mac you will find <RESFolder> in:
 //  Easiest way to get there is to open up the program 'terminal' (use spotlight: ⌘+space)
 //  and then enter: (*note: replace '<Username>' with your username)
@@ -9,7 +11,18 @@
 //  In terminal still, enter:
 //  'open <copiedString>'
 //  and the folder should open in a new window.  Once you are there, open the folder 'modules'
-//  and then open the file 'keyboardNav.js'.
+
+//  Windows:
+//  If you're on a windows machine, open up the program "command line" and enter:
+// 'C:\Users\<Username>\AppData\Local\Google\Chrome\User Data\Default\Extensions'
+//  Using the searchbar in the window type in 'manifest.json'.  Open each one until you find one that has this line:
+//  "description": "Reddit Enhancement Suite - a group of enhancements for browsing Reddit",
+//  or
+//  "name": "Reddit Enhancement Suite",
+//  When you find that file, not the folder it's in.  That's the same folder you will find '/modules/keyboardNav.js'
+
+// Any OS:
+//  Then open the file 'keyboardNav.js'.
 //  Again, paste what you see below to the bottom of the keyboardNav.js file and save.
 //  Then just restart Chrome...
 
