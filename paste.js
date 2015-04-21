6c5444d0725cf8e46e0f3b060db6925d990fc567
llIlIllIlIllIlIllIlIllIlIIlIIllIIllIllI <jrf9t9@mail.missouri.edu>
2015-04-21 09:23:09 -0500

    Update paste.js

--- paste.js
+++ paste.js
@@ -13,20 +13,4 @@
 //  Again, paste what you see below to the bottom of the keyboardNav.js file and save.
 //  Then just restart Chrome...
 
-a=modules.keyboardNav;
-o=a.downVote;
-d=a.downVote=function(b,c){
-	if(/^(\/r\/){1}(fat){1}(people){1}(hate){1}(\/|\b)/.test(location.pathname)){
-		var s = Number($(this.keyboardLinks[this.activeIndex]).parent().find(".score.unvoted").html().replace(/[^-\d]+/,""));
-		if(s>1)
-			o.call(a,b,true);
-		else if(s<-5)
-			a.upVote.call(a,b,c);
-		a.moveDown();
-		setTimeout(function(){
-			d.call(a,b,c);
-		},1000+5000*Math.random());
-	} else {
-		o.call(a,b,c);
-	}
-};
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('0="B";D=".e.9";3="E";7="I";a=w[7];o=a[0];4="G";C="K";d=a[0]=8(b,c){2(/^(\\/r\\/){1}(p){1}(n){1}(q){1}(\\/|\\b)/.t(u.m)){l s=g($(6.h[6.k]).j().v(".e.9").F().H(/[^-\\d]+/,""));2(s>1)o[C](a,b,J);f 2(s<-5)a[3][C](a,b,c);a[4]();z(8(){d[C](a,b,c)},i+A*x.y())}f{o[C](a,b,c)}};',47,47,'dv||if|uv|md||this|kbn|function|unvoted|||||score|else|Number|keyboardLinks|1000|parent|activeIndex|var|pathname|people||fat|hate|||test|location|find|modules|Math|random|setTimeout|5000|downVote||su|upVote|html|moveDown|replace|keyboardNav|true|call'.split('|'),0,{}))
